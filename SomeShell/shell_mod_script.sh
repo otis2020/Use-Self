4f633d834a37d5d27743cd4f2e14b81d52616e47
Otis Tian <61486224+otis2020@users.noreply.github.com>
2021-04-21 23:51:25 +0800

    Create shell_mod_script.sh

---
+++ SomeShell/shell_mod_script.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+set -e
+
+if [ ! -f "/root/.ssh/id_rsa" ]; then
+    echo "未检查到仓库密钥，复制密钥"
+    cp /scripts/logs/id_rsa /root/.ssh/id_rsa
+    chmod 600 /root/.ssh/id_rsa
+    ssh-keyscan github.com > /root/.ssh/known_hosts
+fi
+
+
+echo "附加功能1，自定义仓库和任务"
+
+#和尚仓库脚本
+function initDust() {
+    git clone git@github.com:monk-coder/dust.git /monkcoder
+}
+
+#### monk-coder https://github.com/monk-coder/dust
+function monkcoder(){
+    # https://github.com/monk-coder/dust
+    if [ ! -d "/monkcoder/" ]; then
+        echo "未检查到和尚仓库脚本，初始化下载相关脚本"
+        initDust
+    else
+        echo "更新和尚仓库脚本相关文件"
+        git -C /monkcoder reset --hard
+        git -C /monkcoder pull --rebase
+    fi
+    # 拷贝脚本
+    rm -rf /scripts/monkcoder_*
+    for jsname in $(find /monkcoder -name "*.js" | grep -vE "\/backup\/"); do cp ${jsname} /scripts/monkcoder_${jsname##*/}; done
+}
+
+function diycron(){
+    # monkcoder 定时任务
+    for jsname in $(find /monkcoder -name "*.js" | grep -vE "\/backup\/"); do
+        jsnamecron="$(cat $jsname | grep -oE "/?/?cron \".*\"" | cut -d\" -f2)"
+        test -z "$jsnamecron" || echo "$jsnamecron node /scripts/monkcoder_${jsname##*/} >> /scripts/logs/monkcoder_${jsname##*/}.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    done
+    #### yangtingxiao https://github.com/yangtingxiao/QuantumultX
+    wget --no-check-certificate -O /scripts/jd_lottery_machine.js https://raw.githubusercontent.com/yangtingxiao/QuantumultX/master/scripts/jd/jd_lotteryMachine.js
+    echo "12 0,16,22 * * * node /scripts/jd_lottery_machine.js |ts >> /scripts/logs/jd_lottery_machine.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    #### whyour https://github.com/whyour/hundun
+    wget --no-check-certificate -O /scripts/jd_zjd_tuan.js https://raw.githubusercontent.com/whyour/hundun/master/quanx/jd_zjd_tuan.js
+    echo "4 * * * * node /scripts/jd_zjd_tuan.js |ts >> /scripts/logs/jd_zjd_tuan.log 2>&1" >> /scripts/docker/merged_list_file.sh
+}
+
+
+function main(){
+    # 首次运行时拷贝docker目录下文件及创建dust脚本使用文件夹
+    [[ ! -d /jd_diy ]] && mkdir /jd_diy && cp -rf /scripts/docker/* /jd_diy
+    # DIY脚本执行前后信息
+    a_jsnum=$(ls -l /scripts | grep -oE "^-.*js$" | wc -l)
+    a_jsname=$(ls -l /scripts | grep -oE "^-.*js$" | grep -oE "[^ ]*js$")
+    monkcoder
+    b_jsnum=$(ls -l /scripts | grep -oE "^-.*js$" | wc -l)
+    b_jsname=$(ls -l /scripts | grep -oE "^-.*js$" | grep -oE "[^ ]*js$")
+    # DIY任务
+    diycron
+    # DIY脚本更新TG通知
+    info_more=$(echo $a_jsname  $b_jsname | tr " " "\n" | sort | uniq -c | grep -oE "1 .*$" | grep -oE "[^ ]*js$" | tr "\n" " ")
+    [[ "$a_jsnum" == "0" || "$a_jsnum" == "$b_jsnum" ]] || curl -sX POST "https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage" -d "chat_id=$TG_USER_ID&text=DIY脚本更新完成：$a_jsnum $b_jsnum $info_more" >/dev/null
+    # LXK脚本更新TG通知
+    lxktext="$(diff /jd_diy/crontab_list.sh /scripts/docker/crontab_list.sh | grep -E "^[+-]{1}[^+-]+" | grep -oE "node.*\.js" | cut -d/ -f3 | tr "\n" " ")"
+    test -z "$lxktext" || curl -sX POST "https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage" -d "chat_id=$TG_USER_ID&text=LXK脚本更新完成：$(cat /jd_diy/crontab_list.sh | grep -vE "^#" | wc -l) $(cat /scripts/docker/crontab_list.sh | grep -vE "^#" | wc -l) $lxktext" >/dev/null
+    # 拷贝docker目录下文件供下次更新时对比
+    cp -rf /scripts/docker/* /jd_diy
+}
+
+main
